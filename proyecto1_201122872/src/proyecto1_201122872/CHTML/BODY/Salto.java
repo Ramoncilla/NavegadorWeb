4207ce3ae6f1ad0f35f9e054732400ecf3df2763
Alina <alin2525@gmail.com>
2018-02-15 11:15:21 -0600

    #8 Creacion de las clases con herencia de las etiqueteas vacias

---
+++ proyecto1_201122872/src/proyecto1_201122872/CHTML/BODY/Salto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_201122872.CHTML.BODY;
+import proyecto1_201122872.CHTML.Etiqueta;
+
+/**
+ *
+ * @author Ramonella
+ */
+public class Salto extends Etiqueta {
+    
+}
