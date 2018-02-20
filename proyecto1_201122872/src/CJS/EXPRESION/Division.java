6fade591c52dc5fe5ff6eaa0b260472094637ae6
Alina <alin2525@gmail.com>
2018-02-19 21:21:34 -0600

    #18 Creacion de sumas, restas, multipliciones, divisiones y potencias con sistema de tipos, falta el residuo

--- proyecto1_201122872/src/CJS/EXPRESION/Division.java
+++ proyecto1_201122872/src/CJS/EXPRESION/Division.java
@@ -10,5 +10,60 @@ package CJS.EXPRESION;
  * @author Ramonella
  */
 public class Division  extends expresionBase {
+   
+    
+    public Division(){
+        
+    }
+    
+     @Override
+    public Object Resolver(Object val1, Object val2){
+      
+        double v1= Double.parseDouble(val1.toString());
+        double v2 =Double.parseDouble(val2.toString());
+        
+        String tipo1 = obtenerTipoExpresion(val1);
+        String tipo2 = obtenerTipoExpresion(val2);
+ 
+        String cad1, cad2;
+        double dou1, dou2;
+        
+        /* Validaciones con Booleanos */
+        if (esBool(tipo1) && esNumero(tipo2)) {
+            
+            dou1 = getBool(val1);
+            dou2 = getNumero(val2); 
+            if(dou2==0){
+                return "nulo";
+            }
+            
+           return dou1/dou2;
+           
+        }else  if (esNumero(tipo1) && esBool(tipo2)) {
+            
+            dou1 = getNumero(val1);
+            dou2 = getBool(val2); 
+            if(dou2==0){
+                return "nulo";
+            }
+            
+            return dou1/dou2;
+        }
+        else  if (esNumero(tipo1) && esNumero(tipo2)) {
+            
+            dou1 = getNumero(val1);
+            dou2 = getNumero(val2);  
+            if(dou2==0){
+                return "nulo";
+            }
+            
+            return dou2/dou1;
+        }
+        
+        else {
+            
+           return "nulo"; 
+        }
+    }
     
 }
--- proyecto1_201122872/src/CJS/EXPRESION/Potencia.java
+++ proyecto1_201122872/src/CJS/EXPRESION/Potencia.java
@@ -11,4 +11,52 @@ package CJS.EXPRESION;
  */
 public class Potencia  extends expresionBase {
     
+      public Potencia(){
+        
+    }
+    
+     @Override
+    public Object Resolver(Object val1, Object val2){
+      
+        double v1= Double.parseDouble(val1.toString());
+        double v2 =Double.parseDouble(val2.toString());
+        
+        String tipo1 = obtenerTipoExpresion(val1);
+        String tipo2 = obtenerTipoExpresion(val2);
+ 
+        String cad1, cad2;
+        double dou1, dou2;
+        
+        /* Validaciones con Booleanos */
+        if (esBool(tipo1) && esNumero(tipo2)) {
+            
+            dou1 = getBool(val1);
+            dou2 = getNumero(val2); 
+          
+           return Math.pow(dou1, dou2); 
+           
+        }else  if (esNumero(tipo1) && esBool(tipo2)) {
+            
+            dou1 = getNumero(val1);
+            dou2 = getBool(val2); 
+         
+            
+           return Math.pow(dou1, dou2); 
+        }
+        else  if (esNumero(tipo1) && esNumero(tipo2)) {
+            
+            dou1 = getNumero(val1);
+            dou2 = getNumero(val2);  
+           
+            return Math.pow(dou1, dou2); 
+        }
+        
+        else {
+            
+           return "nulo"; 
+        }
+    }
+    
+    
+    
 }
