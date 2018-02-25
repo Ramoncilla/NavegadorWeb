834fd4c335c17af07f567c480ae2ca075a6b977b
Alina <alin2525@gmail.com>
2018-02-25 07:25:02 -0600

    #36 Ejecucion de un mensaje

--- proyecto1_201122872/src/CJS/ESTRUCURAS_CONTROL/Mensaje.java
+++ proyecto1_201122872/src/CJS/ESTRUCURAS_CONTROL/Mensaje.java
@@ -5,12 +5,35 @@
  */
 package CJS.ESTRUCURAS_CONTROL;
 
+import CJS.TablaSimbolos.tablaSimbolos;
+import CJS.elementoRetorno;
 import CJS.objetoBase;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Ramonella
  */
 public class Mensaje extends objetoBase{
+    
+    public objetoBase expresionMostrar;
+    
+    
+    public Mensaje(Object exp){
+        this.expresionMostrar = (objetoBase)exp;
+    }
+
+    @Override
+    public elementoRetorno Ejecutar(tablaSimbolos tabla, int contexto) {
+        
+        elementoRetorno ret = expresionMostrar.Ejecutar(tabla, contexto);
+        
+        JOptionPane.showMessageDialog(null, ret.ValorRetorno.toString(), "USAC-WEB", JOptionPane.INFORMATION_MESSAGE);
+        return super.Ejecutar(tabla, contexto); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
     
 }
