89971a2ef1ca3d2810f2dce0287c9ae2962b23c6
Alina <alin2525@gmail.com>
2018-02-28 13:29:03 -0600

    #54 Creacion del negativo

--- proyecto1_201122872/src/CJS/ARBOL/EXPRESION/Negativo.java
+++ proyecto1_201122872/src/CJS/ARBOL/EXPRESION/Negativo.java
@@ -5,6 +5,8 @@
  */
 package CJS.ARBOL.EXPRESION;
 
+import CJS.TablaSimbolos.tablaSimbolos;
+import CJS.elementoRetorno;
 import CJS.objetoBase;
 
 /**
@@ -20,6 +22,55 @@ public class Negativo extends objetoBase {
         
         this.expresion = (objetoBase) el;
     }
+   
+    @Override
+    public elementoRetorno Ejecutar(tablaSimbolos tabla, int contexto) {
+
+        elementoRetorno ret = this.expresion.Ejecutar(tabla, contexto);
+        if (ret.ValorRetorno != null) {
+            if (obtenerTipoExpresion(ret.ValorRetorno).equalsIgnoreCase("numero")) {
+
+                double dou = Double.parseDouble(ret.ValorRetorno.toString());
+                ret.ValorRetorno = dou*-1;
+                super.retorno.ValorRetorno = dou*-1;
+
+            } 
+
+        }
+
+        return ret;
+    }
+
+
+
+    private String obtenerTipoExpresion(Object val) {
+
+        if ((val instanceof Double) || (val instanceof Integer)) {
+            return "numero";
+        }
+
+        if (val instanceof String) {
+
+            if (((String) val).equalsIgnoreCase("verdadero")
+                    || ((String) val).equalsIgnoreCase("falso")) {
+                return "bool";
+            } else if (((String) val).equalsIgnoreCase("nulo")) {
+                return "nulo";
+            } else {
+                return "cadena";
+            }
+        }
+
+        if (val instanceof Datee) {
+            return "Date";
+        }
+        if (val instanceof DateTime) {
+            return "DateTime";
+        }
+
+        return "nulo";
+    }
+    
     
     
 }
--- proyecto1_201122872/src/CJS/ARBOL/EXPRESION/Unario.java
+++ proyecto1_201122872/src/CJS/ARBOL/EXPRESION/Unario.java
@@ -13,69 +13,84 @@ import CJS.objetoBase;
  *
  * @author Ramonella
  */
-public class Unario extends objetoBase{
-    
+public class Unario extends objetoBase {
+
     public objetoBase expresionUnario;
     public String operador;
-    
-    public Unario(Object expresion, Object opereandor){
-        
-        this.expresionUnario= (objetoBase)expresion;
-        this.operador=opereandor.toString();
+
+    public Unario(Object expresion, Object opereandor) {
+
+        this.expresionUnario = (objetoBase) expresion;
+        this.operador = opereandor.toString();
     }
 
     @Override
     public elementoRetorno Ejecutar(tablaSimbolos tabla, int contexto) {
-        
-        elementoRetorno ret= this.expresionUnario.Ejecutar(tabla, contexto);
-        if(ret.ValorRetorno!=null){
-            if(obtenerTipoExpresion(ret.ValorRetorno).equalsIgnoreCase("numero")){
-                
+
+        elementoRetorno ret = this.expresionUnario.Ejecutar(tabla, contexto);
+        if (ret.ValorRetorno != null) {
+            if (obtenerTipoExpresion(ret.ValorRetorno).equalsIgnoreCase("numero")) {
+
                 double dou = Double.parseDouble(ret.ValorRetorno.toString());
-                if(operador.equalsIgnoreCase("++")){
+                if (operador.equalsIgnoreCase("++")) {
                     dou++;
-                }else{
+                } else {
                     dou--;
                 }
-                ret.ValorRetorno=dou;
-                super.retorno.ValorRetorno=dou;
-                
+                ret.ValorRetorno = dou;
+                super.retorno.ValorRetorno = dou;
+
+            } else if (obtenerTipoExpresion(ret.ValorRetorno).equalsIgnoreCase("bool")) {
+                double dou = getBoolNumero(ret.ValorRetorno);
+                if (operador.equalsIgnoreCase("++")) {
+                    dou++;
+                } else {
+                    dou--;
+                }
+                ret.ValorRetorno = dou;
+                super.retorno.ValorRetorno = dou;
+
+            }
+
         }
-            
-        }
-         
+
         return ret;
     }
-    
-    
-      private  String obtenerTipoExpresion(Object val) {
-        
-        if ((val instanceof Double)|| (val instanceof Integer)) {
+
+    private Double getBoolNumero(Object val) {
+        if (val.toString().equalsIgnoreCase("verdadero")) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
+
+    private String obtenerTipoExpresion(Object val) {
+
+        if ((val instanceof Double) || (val instanceof Integer)) {
             return "numero";
         }
-       
+
         if (val instanceof String) {
-            
-            if(((String)val).equalsIgnoreCase("verdadero")||
-                    ((String) val).equalsIgnoreCase("falso")){
+
+            if (((String) val).equalsIgnoreCase("verdadero")
+                    || ((String) val).equalsIgnoreCase("falso")) {
                 return "bool";
-            }else if(((String)val).equalsIgnoreCase("nulo")){
-               return "nulo"; 
-            } else{
-                return "cadena"; 
+            } else if (((String) val).equalsIgnoreCase("nulo")) {
+                return "nulo";
+            } else {
+                return "cadena";
             }
         }
-        
-        if(val instanceof Datee){
+
+        if (val instanceof Datee) {
             return "Date";
         }
-        if(val instanceof DateTime ){
+        if (val instanceof DateTime) {
             return "DateTime";
         }
-        
+
         return "nulo";
     }
-     
-    
-    
+
 }
